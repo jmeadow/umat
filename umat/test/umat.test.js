d0363379b2c31ce22f21e71a10469d63a88b15d6
Jeremy Meadow <jeremybmeadow@gmail.com>
2021-05-22 15:03:32 -0400

    add paymentsplitter to investigate code

--- umat/test/umat.test.js
+++ umat/test/umat.test.js
@@ -4,16 +4,22 @@ const assert = require('assert');
 let UMAT;
 let umat;
 let owner;
+let aidWallet;
+let addr1;
+let addr2;
+let addr3;
 let accounts;
 let totalSupply;
 
 describe('UMAT', () => {
 
     beforeEach(async () => {
+        [owner, aidWallet, addr1, addr2, addr3, _] = await ethers.getSigners();
+
         UMAT = await ethers.getContractFactory('UMAT');
         umat = await UMAT.deploy();
-        [owner] = await ethers.getSigners();
-        accounts = await web3.eth.getAccounts();
+        
+        // accounts = await web3.eth.getAccounts();
     });
 
     describe('Deployment tests', () => {
@@ -21,42 +27,84 @@ describe('UMAT', () => {
 
             const umatSymbol = await umat.symbol();
 
-            
-
             assert.ok(umat.address); // it has an address
             assert.equal('UMAT',umatSymbol); // the symbol is UMAT
+
         });
 
-
-        it('total supply is greater than 0 and the owner has the total supply, also we can do math', async () => {
+        it('accounts[0] has the total supply, also we can do math', async () => {
 
             totalSupply = await umat.totalSupply();
             totalSupply = web3.utils.hexToNumberString(totalSupply); // I don't know how to turn the warning off but I'm ignoring it for now
             totalEtherSupply = web3.utils.fromWei(totalSupply, 'ether'); //confirm that we use Wei utils 
 
-            ownerBalance = await umat.balanceOf(owner.address);
+            ownerBalance = await umat.connect(owner).balanceOf(owner.address);
             ownerBalance = web3.utils.hexToNumberString(ownerBalance);
 
 
             assert(totalSupply > 0);
-            assert.equal(totalSupply,ownerBalance);
+            assert.equal(totalSupply, ownerBalance);
             // assert(totalSupply > totalSupply - 1); // I can't get math to work for numbers this large
             assert(totalEtherSupply > totalEtherSupply - 1); // confirm we can do math functions on ether at least
 
-            // putting logs to help confirm things are working right
-            console.log('UMAT address: '+ await umat.address);
-            console.log('owner address: '+ await owner.address);
-            console.log('total wei: '+totalSupply);
-            console.log('total ether: '+totalEtherSupply);
+            // // putting logs to help confirm things are working right
+            // console.log('UMAT address: '+ await umat.address);
+            // console.log('owner address: '+ await owner.address);
+            // console.log('total UMAT wei: '+totalSupply);
+            // console.log('total UMAT: '+totalEtherSupply);
 
         });
+
+        it('there are 4 different accounts with addresses', () => {
+            assert.ok(owner.address);
+            assert.ok(addr1.address);
+            assert.ok(addr2.address);
+            assert.ok(addr3.address);
+            assert.notEqual(owner.address, addr1.address, addr2.address, addr3.address);
+        })
     });
 
 
-    // describe('Basic token transaction tests', () => {
-    //     it('owner can transfer tokens', async () => {
-    //         await 
-    //     });
-    // });
+    describe('Basic token transaction tests', () => {
+        it('owner can transfer 100 wei', async () => {
+            
+            ownerInitialBalance = await umat.connect(owner).balanceOf(owner.address);
+            ownerInitialBalance = web3.utils.hexToNumberString(ownerInitialBalance);
+
+            await umat.connect(owner).transfer(addr1.address, 100);
+
+            addr1Balance = await umat.connect(owner).balanceOf(addr1.address)
+            addr1Balance = web3.utils.hexToNumberString(addr1Balance);
+
+            ownerNewBalance = await umat.connect(owner).balanceOf(owner.address)
+            ownerNewBalance = web3.utils.hexToNumberString(ownerNewBalance);
+
+            assert.equal(100, addr1Balance);
+            assert.equal(ownerInitialBalance - 100, ownerNewBalance);
+
+        });
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
