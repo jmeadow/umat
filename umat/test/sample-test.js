f7dee92d8eac51adeb1e4b2496c9dd4e85ee6f4a
Jeremy Meadow <jeremybmeadow@gmail.com>
2021-05-22 00:10:14 -0400

    welp we got no coins

--- umat/test/sample-test.js
+++ umat/test/sample-test.js
@@ -4,23 +4,38 @@ const assert = require('assert');
 
 let UMAT;
 let umat;
-let umatAddress;
-let umatSymbol;
+let owner;
+
 
 describe('UMAT', () => {
 
     beforeEach(async () => {
         UMAT = await ethers.getContractFactory('UMAT');
         umat = await UMAT.deploy();
+        [owner] = await ethers.getSigners();
     });
 
 
     it('has an address and symbol is UMAT', async () => {
-        const umatAddress = await umat.address;
+
         const umatSymbol = await umat.symbol();
 
-        assert.ok(umatAddress); // it has an address
+        assert.ok(umat.address); // it has an address
         assert.equal('UMAT',umatSymbol); // the symbol is UMAT
     });
+
+
+    it('the owner has a balance of more than 0', async () => {
+        console.log(owner.address);
+        const ownerBalance = await umat.balanceOf(owner.address);
+        console.log(ownerBalance.toNumber());
+
+        assert(ownerBalance > 0);
+        // assert.ok(owner);
+        // assert.ok(owner.address);
+
+    });
+
+
 });
 
