74263e37c947d5b68861e015b5f2e8cc4e228299
Jeremy Meadow <jeremybmeadow@gmail.com>
2021-05-21 23:18:44 -0400

    we're up and running
    lets be happy about that

--- umat/test/sample-test.js
+++ umat/test/sample-test.js
@@ -1,17 +1,19 @@
 const { expect } = require("chai");
 const assert = require('assert');
 
-
 describe('UMAT', () => {
-    it('first test please', async () => {
+    it('has an address and symbol is UMAT', async () => {
+
         const UMAT = await ethers.getContractFactory('UMAT');
         const umat = await UMAT.deploy();
-
+        
         await umat.deployed();
+
+        const umatAddress = umat.address;
         const umatSymbol = await umat.symbol();
-        console.log(umatSymbol);
-        assert.equal('UMAT',umatSymbol); // confirms that an address exists
+
+        assert.ok(umatAddress); // it has an address
+        assert.equal('UMAT',umatSymbol); // the symbol is UMAT
     });
 });
-
 
